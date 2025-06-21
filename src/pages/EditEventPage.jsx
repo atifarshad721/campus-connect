e2e9d4be9b5dfad27ed8cb6b8900fef8c9fd978b
atifarshad721 <atifarshad721@gmail.com>
2025-06-21 12:54:14 +0500

    checking

--- src/pages/EditEventPage.jsx
+++ src/pages/EditEventPage.jsx
@@ -16,6 +16,7 @@ function EditEventPage() {
   const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
+    console.log(BASE_URL);
     const fetchEvent = async () => {
       try {
         const response = await fetch(`${BASE_URL}/events/${id}`);
